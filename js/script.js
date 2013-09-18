da7142c059c5c030a448d18ec8e32c45d9502808
Yekaterina Novikova <yanoviko@ucdavis.edu>
2013-09-18 15:42:04 -0700

    halfway working jquery

--- js/script.js
+++ js/script.js
@@ -3,4 +3,20 @@ $(document).ready(function(){
   $("li a").on("click", function(){
     $(this).children(":first").addClass("active");
   })
+
+  $("li a").on("click", function (event) {
+      event.preventDefault();
+      // hide all elements under crate
+      $('#active').children().hide();
+
+      // now show the element clicked...
+      child_element_name = $(this).data("target");
+      console.log($(this).data("target"));
+      $(child_element_name).fadeToggle();
+  });
+
+  $("li a").hover(function () {
+      $(this).next().fadeToggle();
+  });
+
 })
